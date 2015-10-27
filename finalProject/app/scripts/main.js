b3b11f0b818e5c17e8b5bc9182de045a96f47403
Brian M Asquith <basquithcpt@gmail.com>
2015-10-27 18:11:54 -0400

    added calendar to project

--- finalProject/app/scripts/main.js
+++ finalProject/app/scripts/main.js
@@ -1,1 +1,29 @@
+ var Router = Backbone.Router.extend({
 
+   routes: {
+     "": 'showHome',
+     'repositories': 'showSomething'
+   },
+
+   showHome: function() {
+     $.ajax('external-dragging.html').then(function(page) {
+       $('.content').html(page);
+     });
+   },
+
+   showSomething: function() {
+     $.ajax('index.html').then(function(page) {
+       $('.content').html(page)
+     });
+   },
+
+   initialize: function() {
+     Backbone.history.start();
+   }
+ });
+
+
+
+ $(function() {
+   var router = new Router();
+ });
