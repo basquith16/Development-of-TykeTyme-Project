a4efec2ee0f4be30b1edef55266946777ed7c8a3
Brian M Asquith <basquithcpt@gmail.com>
2015-10-28 16:36:22 -0400

    changed logo

--- finalProject/app/scripts/main.js
+++ finalProject/app/scripts/main.js
@@ -1,9 +1,47 @@
- var Router = Backbone.Router.extend({
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
 
-   routes: {
+
+var Craft = Backbone.Model.extend({
+    url: "/crafts.JSON"
+});
+
+var Crafts = Backbone.Collection.extend({
+  model: Craft,
+  url: '/crafts.JSON'
+
+})
+
+var CraftView = Backbone.View.extend({
+  template: _.template($('#craftTemplate').text()),
+
+  render: function () {
+    console.log('render function')
+    var self = this;
+    this.collection.each(function(craft) {
+      var view = new CraftView({
+        model: craft
+      });
+      self.$el.append(view.render());
+    })
+    return this.$el;
+  }
+});
+
+
+
+//Router
+
+var Router = Backbone.Router.extend({
+
+  routes: {
      "": 'showHome',
-     'repositories': 'showSomething'
-   },
+     'crafts': 'showCrafts',
+     'activities': 'showActivities',
+     'whatsAround': 'showWhatsAround',
+     'meals': 'showMeals'
+ },
 
    showHome: function() {
      $.ajax('external-dragging.html').then(function(page) {
@@ -11,19 +49,35 @@
      });
    },
 
-   showSomething: function() {
-     $.ajax('index.html').then(function(page) {
-       $('.content').html(page)
+   showCrafts: function() {
+     var craft = new Craft({
      });
+
+     console.log(craft);
+
+     craft.fetch().then(function () {
+       console.log('fetched');
+       var view = new CraftView({
+         model: craft
+       });
+       $('.fc-events').html(view.render());
+     })
    },
 
+  //  showActivities: function() {
+  //  },
+
    initialize: function() {
+     console.log('initialized');
+     var craft = new Craft();
+     collection: this.crafts
      Backbone.history.start();
    }
- });
+  });
 
 
 
- $(function() {
-   var router = new Router();
- });
+$(function() {
+  console.log('router initialized')
+ var router = new Router();
+});
