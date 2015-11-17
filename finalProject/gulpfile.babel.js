650c837a622315b5db85e5746d96fa6a1f6ff3bb
Brian M Asquith <basquithcpt@gmail.com>
2015-11-16 22:12:11 -0500

    syntax error

--- finalProject/gulpfile.babel.js
+++ finalProject/gulpfile.babel.js
@@ -167,5 +167,3 @@ gulp.task('default', ['clean'], () => {
 
 var ghpages = require('gh-pages');
 var path = require('path');
-
-ghpages.publish(path.join(__dirname, 'dist'), function(err) { ... });
